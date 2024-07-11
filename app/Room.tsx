924a65a9c0dc5662819f9fc1785a5060c60f9d92
markoscomk <markoscomks@gmail.com>
2024-07-11 14:34:07 +0100

    Start setup for live-canvas project

---
+++ app/Room.tsx
@@ -0,0 +1,20 @@
+"use client";
+
+import { ReactNode } from "react";
+import {
+  LiveblocksProvider,
+  RoomProvider,
+  ClientSideSuspense,
+} from "@liveblocks/react/suspense";
+
+export function Room({ children }: { children: ReactNode }) {
+  return (
+    <LiveblocksProvider publicApiKey={"pk_dev_-rjdePeoS60K0pJsgxVMGqdau_D9Tpq3QEfmCEDLasEyUbTdIP3myqGUZklISkWw"}>
+      <RoomProvider id="my-room">
+        <ClientSideSuspense fallback={<div>Loading…</div>}>
+          {children}
+        </ClientSideSuspense>
+      </RoomProvider>
+    </LiveblocksProvider>
+  );
+}
